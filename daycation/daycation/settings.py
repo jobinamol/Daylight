5c34bcd441d313434c63e88d802da1c40c6bcb4b
jobinamol <jobinamoljaimon@gmail.com>
2024-08-29 23:20:27 +0530

    "Updated settings.py to use MySQL database, and modified packages.html template to change image and package name."

--- daycation/daycation/settings.py
+++ daycation/daycation/settings.py
@@ -119,13 +119,16 @@ WSGI_APPLICATION = 'daycation.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'UserDB',
+        'USER': 'root',
+        'PASSWORD': 'Jobina@123*',
+        'HOST': 'localhost',
+        'PORT': '3306',
+
+
 
 
 # Password validation
