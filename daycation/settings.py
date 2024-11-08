1526701cd45878e386d7127c79dfd94d5a5d6311
jobinamol <jobinamoljaimon@gmail.com>
2024-11-08 14:21:07 +0530

    final mini project works

--- daycation/settings.py
+++ daycation/settings.py
@@ -152,32 +152,32 @@ WSGI_APPLICATION = 'daycation.wsgi.application'
 # MySQL Database Configuration
 pymysql.install_as_MySQLdb()
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': os.getenv('DB_NAME', 'DRMS'),
-#         'USER': os.getenv('DB_USER', 'jobina'),
-#         'PASSWORD': os.getenv('DB_PASSWORD', '1234'),
-#         'HOST': os.getenv('DB_HOST', 'localhost'),
-#         'PORT': os.getenv('DB_PORT', '3306'),
-#         'OPTIONS': {
-#             'charset': 'utf8mb4',
-#         },
-#     }
-# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'DRMS_kitchenwe',  # Your database name
-        'USER': 'DRMS_kitchenwe',  # Your database user
-        'PASSWORD': 'e63c87edeef1a253b38a633793929686772a708a',  # Your database password
-        'HOST': 'w5mbd.h.filess.io',  # Your database host
-        'PORT': '3307',  # Your database port
+        'NAME': os.getenv('DB_NAME', 'DRMS'),
+        'USER': os.getenv('DB_USER', 'jobina'),
+        'PASSWORD': os.getenv('DB_PASSWORD', '1234'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '3306'),
         'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
         },
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'DRMS_kitchenwe',  # Your database name
+#         'USER': 'DRMS_kitchenwe',  # Your database user
+#         'PASSWORD': 'e63c87edeef1a253b38a633793929686772a708a',  # Your database password
+#         'HOST': 'w5mbd.h.filess.io',  # Your database host
+#         'PORT': '3307',  # Your database port
+#         'OPTIONS': {
+#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+#         },
+#     }
+# }
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
